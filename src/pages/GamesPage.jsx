b974326b9464fbc095664195276699e068408c0c
LoujinGomez <loujingomez12@gmail.com>
2024-05-15 10:21:16 +0800

    Update GamesPage.jsx

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -1,32 +1,12 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import GamesList from '../components/GamesList';
-
-function GamesPage({ addToWishlist, searchQuery }) {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    fetchGames();
-  }, []);
-
-  const fetchGames = async () => {
-    const response = await fetch('/api/games');
-    const data = await response.json();
-    setGames(data);
-  };
-
-  const filteredGames = games.filter(game =>
-    game.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
+import PopularGames from '../components/PopularGames';
+const GamesPage = ({ addToWishlist, searchQuery }) => {
   return (
-    <div>
-      {filteredGames.map(game => (
-        <div key={game.id}>
-          <h3>{game.name}</h3>
-          <button onClick={() => addToWishlist(game)}>Add to Wishlist</button>
-        </div>
-      ))}
-    </div>
+      <div>
+      <PopularGames onAddToWishlist={addToWishlist} />
+      <GamesList onAddToWishlist={addToWishlist} searchQuery={searchQuery}/>
+      </div>
   );
 }
 
